500afa8b12748b115b896b6d423e822b225eabc8
Vishal <chinchanevishal03@gmail.com>
2021-12-12 08:48:32 +0530

    created

---
+++ script.js
@@ -0,0 +1,73 @@
+console.log("hello uncle");
+
+// Todos"
+// 1. Store all the data to the localStorage
+// 2. Give another column as an option to delete the book
+// 3. Add a scroll bar to the view
+
+// Constructor
+function Book(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+// Display Constructor  (abb isse hum constructor ko show karenge nam, authour our type ko.)
+function Display() {
+
+}
+
+
+
+
+
+// Add methods to display prototype( ab hum  jo method add krenge usse prototype ko dikhaynge.)
+// Display.prototype.add = function(){
+//     console.log('adding to UI')
+// }
+
+// Display.prototype.clear = function(){
+//     let libraryForm = document.getElementById('libraryForm');
+//     libraryForm.reset();
+// }
+
+
+
+
+
+
+// Add submit event listener to libraryForm
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    console.log('jai ho print this');
+
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
+    let type;
+
+
+
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let cooking = document.getElementById('cooking');
+
+    if (fiction.checked) {
+        type = fiction.value;
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (cooking.checked) {
+        type = cooking.value;
+    }
+
+    let book = new Book(name, author, type);
+    console.log(book);
+
+    // let display = new display();
+    // display.add(book);
+    // display.clear();
+    e.preventDefault();
+}
