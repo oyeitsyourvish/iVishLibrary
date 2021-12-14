cb72aa26d953060f3528e53f52475f3a773d474b
Vishal <chinchanevishal03@gmail.com>
2021-12-14 08:54:06 +0530

    addedDisplayShow

--- script.js
+++ script.js
@@ -22,15 +22,46 @@ function Display() {
 
 
 // Add methods to display prototype( ab hum  jo method add krenge usse prototype ko dikhaynge.)
-Display.prototype.add = function(){
-    console.log('adding to UI')
+Display.prototype.add = function (book) {
+    console.log('adding to UI');
+    let tableBody = document.getElementById('tableBody');
+    let uiString = `<tr>
+                        <td>${book.name}</td>
+                        <td>${book.author}</td>
+                        <td>${book.type}</td>
+                    </tr>`;
+    tableBody.innerHTML += uiString;
+
 }
 
-Display.prototype.clear = function(){
+// Implement the clear function
+Display.prototype.clear = function () {
     let libraryForm = document.getElementById('libraryForm');
     libraryForm.reset();
 }
 
+
+// Implement the validate function
+Display.prototype.validate = function (book) {
+    if (book.name.length < 2 || book.author.length < 2) {
+        return false
+    }
+    else {
+        return true;
+    }
+}
+Display.prototype.show = function (type, displayMessage) {
+    let message = document.getElementById('message');
+    message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                            <strong>Messge:</strong> ${displayMessage}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                            </button>
+                        </div>`;
+                        setTimeout(function(){
+                            message.innerHTML = ''
+                        },2000);
+}
 
 
 
@@ -67,7 +98,13 @@ function libraryFormSubmit(e) {
     console.log(book);
 
     let display = new Display();
-    display.add(book);
-    display.clear();
+    if (display.validate(book)){
+        display.add(book);
+        display.clear();
+        display.show('success', 'Your book has been successfully added.');
+    }
+    else{
+        display.show('danger', 'Sorry you cannot add this book try later');
+    }
     e.preventDefault();
 }
